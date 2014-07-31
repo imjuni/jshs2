b0a08ab5afe657240aafb298cfe3ec9efc13d88d
{bjlee} <{bjlee@sk.com}>
2014-07-31 17:57:24 +0900

    Reval hs2error. Because, you detect error by instanceof operator. Maybe caused from connection, err object is match HS2Error.ConnectionError.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 exports.Connections = require('./lib/Connections')();
 exports.Cursor = require('./lib/Cursor')();
 exports.HS2Util = require('./lib/HS2Util');
+exports.HS2Error = require('./lib/HS2Error')();
